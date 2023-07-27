5b572ed65d27c43775997f4ee8d16831887c6ca9
Danuz05 <137125134+Danuz05@users.noreply.github.com>
2023-07-27 02:35:03 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const responsive = document.querySelector(".toggle")
+const responsive2  = document.querySelector(".nav")
+responsive.addEventListener("click", ()=> {
+responsive2.classList.toggle("nav-active")
+})
+
